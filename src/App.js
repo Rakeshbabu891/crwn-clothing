8c9552c7af142903a9cc48bbbe7f2f5b090ef547
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2020-07-18 16:08:28 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+const HatPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hat" component={HatPage} />
+      </Switch>
     </div>
   );
 }
